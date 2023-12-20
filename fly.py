8011632307667b2879869fcff557b0021090071f
ernests1c <149561992+ernests1c@users.noreply.github.com>
2023-12-20 08:51:44 +0200

    Update fly.py

--- fly.py
+++ fly.py
@@ -17,16 +17,21 @@ tello.connect()
 tello.takeoff()
 
 tello.set_speed(20)
-
-tello.move_left(100)
-tello.move_right(100)
-tello.move_forward(100)
-tello.move_back(100)
-
-tello.land()
-tello.takeoff()
-
+tello.move_up(75)
+tello.rotate_clockwise(180)
+tello.move_forward(122)
+tello.move_down(75)
+tello.move_up(75)
+tello.rotate_counter_clockwise(90)
+tello.move_forward(75)
+tello.rotate_clockwise(90)
+tello.move_forward(64)
+tello.move_down(75)
+tello.move_up(75)
+tello.rotate_clockwise(90)
+tello.move_forward(75)
+# 让每一架Tello单独执行不同的操作
 tello.rotate_clockwise(360)
 tello.rotate_counter_clockwise(360)
-
+tello.move_down(75)
 tello.land()
